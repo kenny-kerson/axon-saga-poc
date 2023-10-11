6512b9da9d2e0f4b37a85cdcbeb25f73e1743c3c
kenny-kerson <kenny.kerson@gmail.com>
2023-10-11 23:06:33 +0900

    Initial Commit
    - 프로젝트 최초 생성

---
+++ src/main/java/com/kenny/axonsagapoc/AxonSagaPocApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.axonsagapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonSagaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonSagaPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenny/axonsagapoc/AxonSagaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.axonsagapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonSagaPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
